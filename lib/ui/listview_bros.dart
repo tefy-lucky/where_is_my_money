5735de2cb84c8ae2683015907200ab3a68bf6226
trakotom <trakotom@cegid.com>
2020-02-04 16:32:47 +0300

    finally I understand how UI in flutter works. not all but hey, it's a good start no? thank you hon

--- lib/ui/listview_bros.dart
+++ lib/ui/listview_bros.dart
@@ -1,62 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:where_is_my_money/model/bro.dart';
+import 'package:where_is_my_money/ui/update_bro_screen.dart';
 import 'package:where_is_my_money/util/database_helper.dart';
-
-//class ListViewBros extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Where is my money',
-//      theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
-//      home: Scaffold(
-//        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-//        appBar: AppBar(
-//          title: Text('List'),
-//          centerTitle: true,
-//        ),
-//        body: Container(
-//          child: ListView.builder(
-//            scrollDirection: Axis.vertical,
-//            shrinkWrap: true,
-//            itemCount: 10,
-//            itemBuilder: (BuildContext context, int index) {
-//              return Card(
-//                elevation: 8.0,
-//                margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-//                child: Container(
-//                  decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-//                  child: ListTile(
-//                    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-//                    leading: Container(
-//                      padding: EdgeInsets.only(right: 12.0),
-//                      decoration: new BoxDecoration(
-//                        border: new Border(
-//                          right: new BorderSide(width: 1.0, color: Colors.white24)
-//                        )
-//                      ),
-//                      child: Icon(Icons.person, color: Colors.white, size: 30.0),
-//                    ),
-//                    title: Text(
-//                      "Person 1",
-//                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-//                    ),
-//                    subtitle: Row(
-//                      children: <Widget>[
-//                        Icon(Icons.account_balance_wallet, color: Colors.yellowAccent),
-//                        Text(" 5,000 Ar", style: TextStyle(color: Colors.white)),
-//                      ],
-//                    ),
-//                    trailing: Icon(Icons.delete, color: Colors.white, size: 30.0),
-//                  ),
-//                ),
-//              );
-//            },
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
+import 'package:where_is_my_money/util/ui.dart';
 
 class ListViewBros extends StatefulWidget {
   @override
@@ -91,10 +37,7 @@ class _ListViewBros extends State<ListViewBros> {
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
       home: Scaffold(
         backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-        appBar: AppBar(
-          title: Text('List'),
-          centerTitle: true,
-        ),
+        appBar: getDefaultAppBar('List'),
         body: Container(
           child: ListView.builder(
             scrollDirection: Axis.vertical,
@@ -109,38 +52,36 @@ class _ListViewBros extends State<ListViewBros> {
                   decoration:
                       BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
                   child: ListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 10.0),
-                      leading: Container(
-                        padding: EdgeInsets.only(right: 12.0),
-                        decoration: new BoxDecoration(
-                            border: new Border(
-                                right: new BorderSide(
-                                    width: 1.0, color: Colors.white24
-                                )
-                            )
-                        ),
-                        child:
-                            Icon(Icons.person, color: Colors.white, size: 30.0),
-                      ),
-                      title: Text(
-                        '${items[index].name}',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Row(
-                        children: <Widget>[
-                          Icon(Icons.account_balance_wallet,
-                              color: Colors.yellowAccent),
-                          Text('${items[index].amount}',
-                              style: TextStyle(color: Colors.white)),
-                        ],
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete,
-                            color: Colors.white, size: 30.0),
-                        onPressed: () => {},
-                      )
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    leading: Container(
+                      padding: EdgeInsets.only(right: 12.0),
+                      decoration: new BoxDecoration(
+                          border: new Border(
+                              right: new BorderSide(
+                                  width: 1.0, color: Colors.white24))),
+                      child:
+                          Icon(Icons.person, color: Colors.white, size: 30.0),
+                    ),
+                    title: Text(
+                      '${items[index].name}',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Row(
+                      children: <Widget>[
+                        Icon(Icons.account_balance_wallet,
+                            color: Colors.yellowAccent),
+                        Text('${items[index].amount}',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete,
+                          color: Colors.white, size: 30.0),
+                      onPressed: () => {},
+                    ),
+                    onTap: () => _navigateToEditPage(context, items[index]),
                   ),
                 ),
               );
@@ -149,9 +90,23 @@ class _ListViewBros extends State<ListViewBros> {
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () => {},
+          onPressed: () => _new(context),
         ),
       ),
     );
   }
+}
+
+void _navigateToEditPage(BuildContext context, Bro bro) async {
+  await Navigator.push(context, MaterialPageRoute(builder: (context) => BroScreen(bro)));
+}
+
+void _new(BuildContext context) async {
+  final Bro bro = Bro(
+    id: 0,
+    name: '',
+    amount: 0.0,
+    isPaid: false
+  );
+  await Navigator.push(context, MaterialPageRoute(builder: (context) => BroScreen(bro)));
 }
---
+++ lib/ui/update_bro_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:where_is_my_money/model/bro.dart';
+import 'package:where_is_my_money/util/database_helper.dart';
+import 'package:where_is_my_money/util/ui.dart';
+
+class BroScreen extends StatefulWidget {
+  final Bro bro;
+  BroScreen(this.bro);
+
+  @override
+  State<StatefulWidget> createState() => new _BroScreenState();
+}
+
+class _BroScreenState extends State<BroScreen> {
+  DatabaseHelper db = new DatabaseHelper();
+
+  TextEditingController _nameController;
+  TextEditingController _amountController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _nameController = new TextEditingController(text: '${widget.bro.name}');
+    _amountController = new TextEditingController(text: '${widget.bro.amount}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: getDefaultAppBar(''),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: new InputDecoration(labelText: "Name", icon: Icon(Icons.person)),
+            ),
+            Padding(padding: new EdgeInsets.all(5.0),),
+            TextField(
+              controller: _amountController,
+              decoration: new InputDecoration(labelText: "Amount", icon: Icon(Icons.attach_money)),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
