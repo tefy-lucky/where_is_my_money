947deded949afae66c56873717d9c48ebb7ab980
trakotom <tefy.rakotomahefa@gmail.com>
2020-02-06 15:05:34 +0300

    showAlert function

--- lib/util/ui.dart
+++ lib/util/ui.dart
@@ -6,3 +6,23 @@ AppBar getDefaultAppBar(String title) {
     centerTitle: true,
   );
 }
+
+void showAlert(BuildContext context, String title, String content) {
+  showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      });
+}
